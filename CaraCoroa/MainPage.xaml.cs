85d4827fcbabaffc8554dc9687e0a4ba3b96723d
gfferreiira <arzhun7381@gmail.com>
2024-04-01 22:54:30 -0300

    Finalização do metodo random

--- CaraCoroa/MainPage.xaml.cs
+++ CaraCoroa/MainPage.xaml.cs
@@ -11,16 +11,16 @@
 
          private void BtnSpin_Clicked(object sender, EventArgs e)
         {
-            int valor = 0;
+            string valor = "";
 
             //Pegar o valor que foi selecionado pelo usuario no picker
-            valor = Convert.ToInt32(SidesPicker.SelectedItem);
+            valor = Convert.ToString(SidesPicker.SelectedItem);
             //Jogar esse valor em uma variavel
             // sortear um numero em um dado utilizando o Random
             // Colocar o valor de uma varíavel 
             // Exibir o valor para o usuário na SpinCountLabel
             int valorSorteado = 0;
-            valorSorteado = new Random().Next(1, valor);
+            valorSorteado = new Random().Next(0, 2);
             if (valorSorteado == 0)
             {
                 ResultLabel.Text = "Cara";
